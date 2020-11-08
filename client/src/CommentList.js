0fe6227358a58158b4d6e31df74556fba901fe31
Bruno Moura <bruno8moura@gmail.com>
2020-11-08 17:16:41 -0300

    Refactory: Arrow function with a default return.

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -6,13 +6,11 @@ export default ({ comments }) => {
             return (<li key={comment.id}>{comment.content}</li>);
         }
 
-        if(comment.status === 'pending'){
-            return (<li key={comment.id}>{'This comment is awaiting moderation.'}</li>);
-        }
-        
         if(comment.status === 'rejected'){
             return (<li key={comment.id}>{'This comment has been rejected.'}</li>);
         }
+
+        return (<li key={comment.id}>{'This comment is awaiting moderation.'}</li>);        
     });
 
     return (
